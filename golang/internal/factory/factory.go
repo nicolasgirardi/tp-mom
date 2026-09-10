2976de2b8454e3c66c4d1711d424e5b7f557d935
Nicolas Girardi <ngirardi@fi.uba.ar>
2026-09-10 15:03:54 -0300

    avanzo con queue middleware

--- golang/internal/factory/factory.go
+++ golang/internal/factory/factory.go
@@ -1,36 +1,117 @@
 package factory
 
 import (
+	"context"
 	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
 
 	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SecureString struct {
+	text string
+	mu   sync.Mutex
+}
+
+func NewSecureString(text string) *SecureString {
+	return &SecureString{text: text}
+}
+
+func (s *SecureString) Compare(text string) bool {
+	return s.text == text
+}
+
+func (s *SecureString) Text() string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.text
+}
+
+func (s *SecureString) Store(newText string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.text = newText
+}
+
 type MyQueueMiddleware struct {
-	myConnection *amqp.Connection
-	myChannel    *amqp.Channel
-	myQueue      amqp.Queue
+	myConnection  *amqp.Connection
+	myChannel     *amqp.Channel
+	myQueue       amqp.Queue
+	myConsumerTag *SecureString
+	consuming     bool
+	mutex         sync.Mutex
 }
 
-func (m MyQueueMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack func(), nack func())) error {
-	//TODO implement me
-	panic("implement me")
+func (mQ *MyQueueMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack func(), nack func())) error {
+	mQ.mutex.Lock()
+	mQ.consuming = true
+	mQ.mutex.Unlock()
+	msgs, err := mQ.myChannel.Consume(
+		mQ.myQueue.Name, // queue
+		mQ.myConsumerTag.Text(),
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return m.ErrMessageMiddlewareDisconnected
+	}
+	firstMessage := true
+	for msg := range msgs {
+		if firstMessage {
+			mQ.myConsumerTag.Store(msg.ConsumerTag)
+		}
+		firstMessage = false
+		message := m.Message{Body: string(msg.Body)}
+		ack := func() { _ = msg.Ack(false) }
+		nack := func() { _ = msg.Nack(false, true) }
+		callbackFunc(message, ack, nack)
+	}
+	mQ.mutex.Lock()
+	defer mQ.mutex.Unlock()
+	if mQ.consuming {
+		return m.ErrMessageMiddlewareDisconnected
+	}
+	return nil
 }
 
-func (m MyQueueMiddleware) StopConsuming() error {
-	//TODO implement me
-	panic("implement me")
+func (mQ *MyQueueMiddleware) StopConsuming() error {
+	if !mQ.consuming {
+		return nil
+	}
+	consumerTag := mQ.myConsumerTag.Text()
+	mQ.mutex.Lock()
+	defer mQ.mutex.Unlock()
+	mQ.consuming = false
+	err := mQ.myChannel.Cancel(consumerTag, false)
+	if err != nil {
+		return m.ErrMessageMiddlewareDisconnected
+	}
+	return nil
 }
 
-func (m MyQueueMiddleware) Send(msg m.Message) error {
-	//TODO implement me
-	panic("implement me")
+func (mQ *MyQueueMiddleware) Send(msg m.Message) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := mQ.myChannel.PublishWithContext(ctx, "", mQ.myQueue.Name, false, false, amqp.Publishing{ContentType: "text/plain", Body: []byte(msg.Body)})
+	if err != nil {
+		return m.ErrMessageMiddlewareDisconnected
+	}
+	return nil
 }
 
-func (m MyQueueMiddleware) Close() error {
-	//TODO implement me
-	panic("implement me")
+func (mQ *MyQueueMiddleware) Close() error {
+	errC := mQ.myConnection.Close()
+	errQ := mQ.myChannel.Close()
+	if errC != nil || errQ != nil {
+		return m.ErrMessageMiddlewareClose
+	}
+	return nil
 }
 
 func CreateQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
@@ -51,9 +132,14 @@ func CreateQueueMiddleware(queueName string, connectionSettings m.ConnSettings) 
 		conn.Close()
 		return nil, err
 	}
+	var consuming atomic.Bool
+	consuming.Store(false)
 	aMiddleWare := &MyQueueMiddleware{
-		myConnection: conn,
-		myQueue:      queue,
+		myConnection:  conn,
+		myChannel:     channel,
+		myQueue:       queue,
+		myConsumerTag: NewSecureString(""),
+		consuming:     false,
 	}
 	return aMiddleWare, nil
 }
@@ -64,24 +150,27 @@ type MyExchangeMiddleware struct {
 	myKeys       []string
 }
 
-func (m MyExchangeMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack func(), nack func())) error {
+func (mE *MyExchangeMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack func(), nack func())) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m MyExchangeMiddleware) StopConsuming() error {
+func (mE *MyExchangeMiddleware) StopConsuming() error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m MyExchangeMiddleware) Send(msg m.Message) error {
+func (mE *MyExchangeMiddleware) Send(msg m.Message) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m MyExchangeMiddleware) Close() error {
-	//TODO implement me
-	panic("implement me")
+func (mE *MyExchangeMiddleware) Close() error {
+	err := mE.myConnection.Close()
+	if err != nil {
+		return m.ErrMessageMiddlewareClose
+	}
+	return nil
 }
 
 func CreateExchangeMiddleware(exchange string, keys []string, connectionSettings m.ConnSettings) (m.Middleware, error) {
